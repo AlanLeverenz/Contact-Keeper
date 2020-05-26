3a320eb003f25d2b28b7f72db2b4c7cd1f9e981f
Alan Leverenz <awleverenz@aol.com>
2020-05-25 21:37:11 -0400

    applied JWT to users.js, receiving token from Mongo Atlas

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -48,7 +50,23 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      // JSON Web Token, option: expires in seconds, callback to get token
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');

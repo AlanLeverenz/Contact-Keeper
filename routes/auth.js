3e11714b6448e8f64733b5c9d5b729e649324ec8
Alan Leverenz <awleverenz@aol.com>
2020-05-26 13:12:23 -0400

    setup authentication for users and auth files

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
 
+// validate the user
 router.get('/', (req, res) => {
   res.send('Get logged in user');
 });
@@ -13,8 +20,56 @@ router.get('/', (req, res) => {
 // @desc    Auth user and get token
 // @access  Public
 
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+// authenticate the user
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  // handle errors
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid redentials' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      // JSON Web Token, option: expires in seconds, callback to get token
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+);
 
 module.exports = router;

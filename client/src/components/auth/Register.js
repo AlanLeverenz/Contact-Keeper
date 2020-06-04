78e68e796d3a8b4e627baf2bac4cf2cd4b6079ca
Alan Leverenz <awleverenz@aol.com>
2020-06-03 22:48:56 -0400

    added Register and Login components

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -8,14 +8,21 @@ const Register = () => {
     password2: '',
   });
 
-  const { name, email, password, password1 } = user;
+  const { name, email, password, password2 } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Register submit');
+  };
 
   return (
     <div className='form-container'>
       <h1>
         Account <span className='text-primary'>Register</span>
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div classNAme='form-group'>
           <label htmlFor='name'>Name</label>
           <input type='text' name='name' value={name} onChange={onChange} />
@@ -36,9 +43,9 @@ const Register = () => {
         <div classNAme='form-group'>
           <label htmlFor='password2'>Confirm Password</label>
           <input
-            type='password2'
+            type='password'
             name='password2'
-            value={password}
+            value={password2}
             onChange={onChange}
           />
         </div>
@@ -51,3 +58,5 @@ const Register = () => {
     </div>
   );
 };
+
+export default Register;

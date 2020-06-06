918317ad973ca7564ea04b036917eb3191a68ccf
Alan Leverenz <awleverenz@aol.com>
2020-06-06 14:26:55 -0400

    added user login, check on user in state

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -19,7 +19,9 @@ export default (state, action) => {
         user: action.payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
+      console.log('USER LOADED: ' + action.payload);
       return {
         ...state,
         ...action.payload,
@@ -28,6 +30,7 @@ export default (state, action) => {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,

37cf8f24cf5188c53b0eceaca3a9dfdb8c090d35
Alan Leverenz <awleverenz@aol.com>
2020-06-07 18:54:15 -0400

    added Get and Clear Contacts

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -21,7 +21,6 @@ export default (state, action) => {
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
-      console.log('USER LOADED: ' + action.payload);
       return {
         ...state,
         ...action.payload,
